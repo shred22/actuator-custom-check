619d32d45f10991027c8981820c75e9ba2d63807
shred22 <shreyas.dange22@gmail.com>
2021-05-05 20:01:17 +0530

    Adding local maven repo publish capabilities

---
+++ src/main/java/com/actuator/custom/check/ACtuatorCustomCheckApp.java
@@ -0,0 +1,11 @@
+package com.actuator.custom.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ACtuatorCustomCheckApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ACtuatorCustomCheckApp.class);
+    }
+}
